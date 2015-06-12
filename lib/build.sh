0cb639f1bc09bd1c2d816a8ea064953c9d4f6819
Gabriel Jaldon <gjaldon85@gmail.com>
2015-06-13 03:15:55 +0800

    Compile installs npm

--- lib/build.sh
+++ lib/build.sh
@@ -9,3 +9,13 @@ install_node() {
   chmod +x $heroku_path/node/bin/*
   PATH=$heroku_path/node/bin:$PATH
 }
+
+install_npm() {
+  # Optionally bootstrap a different npm version
+  if [[ `npm --version` == "$npm_version" ]]; then
+    info "npm `npm --version` already installed with node"
+  else
+    info "Downloading and installing npm $npm_version (replacing version `npm --version`)..."
+    npm install --unsafe-perm --quiet -g npm@$npm_version 2>&1 >/dev/null | indent
+  fi
+}

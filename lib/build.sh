49d7a1c3bcafa8a57b50d29ae3d12dbc8694e1c0
Pratik Khadloya <tispratik@gmail.com>
2015-11-28 07:12:11 -0800

    Install sass during compilation
    This is needed so that we can support the case when some of the scss files import @compass
    Refer: https://github.com/brunch/sass-brunch/issues/5

--- lib/build.sh
+++ lib/build.sh
@@ -107,3 +107,24 @@ write_profile() {
                      export MIX_ENV=${MIX_ENV}"
   echo $export_line >> $build_dir/.profile.d/phoenix_static_buildpack_paths.sh
 }
+
+install_sass() {
+  export GEM_HOME=$build_dir/.gem/ruby/2.2.0
+  export PATH=$GEM_HOME/bin:$PATH
+
+  if test -d $cache_dir/ruby/.gem; then
+    info "Restoring ruby gems directory from cache"
+    cp -r $cache_dir/ruby/.gem $build_dir
+    HOME=$build_dir gem update sass --user-install --no-rdoc --no-ri
+  else
+    HOME=$build_dir gem install sass --user-install --no-rdoc --no-ri
+  fi
+
+  rm -rf $cache_dir/ruby
+  mkdir -p $cache_dir/ruby
+
+  if test -d $build_dir/.gem; then
+    info "Caching ruby gems directory for future builds"
+    cp -r $build_dir/.gem $cache_dir/ruby
+  fi
+}

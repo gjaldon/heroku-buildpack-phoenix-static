f7deb686bf28d66faeac84d3aeb7a00d9b8079cf
Ricardo Carrasco Cursach <rccursach@gmail.com>
2019-03-12 22:58:09 -0300

    phoenix 1.3.x has a package.json inside assets dir

--- lib/common.sh
+++ lib/common.sh
@@ -30,19 +30,16 @@ load_config() {
   # Source for default versions file from buildpack first
   source "${build_pack_dir}/phoenix_static_buildpack.config"
 
-  if [ -f $custom_config_file ]; then
-    source $custom_config_file
-  else
-    info "WARNING: phoenix_static_buildpack.config wasn't found in the app"
-    info "Using default config from Phoenix static buildpack"
-  fi
-
   phoenix_dir=$build_dir/$phoenix_relative_path
 
   info "Detecting assets directory"
   if [ -f "$phoenix_dir/$assets_path/package.json" ]; then
     # Check phoenix custom sub-directory for package.json
-    info "* package.json found in custom directory"
+    info "* package.json found in assets directory"
+    info "* assuming phoenix 1.3.x and later, please check config file"
+    
+    assets_path=assets
+    phoenix_ex=phx
   elif [ -f "$phoenix_dir/package.json" ]; then
     # Check phoenix root directory for package.json, phoenix 1.2.x and prior
     info "WARNING: package.json detected in root "
@@ -59,7 +56,16 @@ load_config() {
     phoenix_ex=phx
   fi
 
+  # Override vars from custom config file if present
+  if [ -f $custom_config_file ]; then
+    source $custom_config_file
+  else
+    info "WARNING: phoenix_static_buildpack.config wasn't found in the app"
+    info "Using default config from Phoenix static buildpack"
+  fi
+
   assets_dir=$phoenix_dir/$assets_path
+
   info "Will use phoenix configuration:"
   info "* assets path ${assets_path}"
   info "* mix tasks namespace ${phoenix_ex}"

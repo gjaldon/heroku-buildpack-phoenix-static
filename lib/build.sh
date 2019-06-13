15f71e3e08e8b1fad440801293861b6d3bf01306
Justin Smestad <justin.smestad@gmail.com>
2019-06-13 13:41:10 +0200

    Add .npmrc to prune command and NPM_TOKEN .npmrc generation

--- lib/build.sh
+++ lib/build.sh
@@ -1,3 +1,5 @@
+
+ 
 cleanup_cache() {
   if [ $clean_cache = true ]; then
     info "clean_cache option set to true."
@@ -134,6 +136,13 @@ install_and_cache_deps() {
 }
 
 install_npm_deps() {
+  npm_token="${NPM_TOKEN:-$(cat $env_dir/NPM_TOKEN)}"
+
+  if [ "$npm_token" != "" ]; then
+    info "Adding npm registry entry to .npmrc using environment variable NPM_TOKEN"
+    echo "//registry.npmjs.org/:_authToken=${npm_token}" >> "${build_dir}/.npmrc"
+  fi
+  npm prune --userconfig $build_dir/.npmrc
   npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1 | indent
   npm rebuild 2>&1 | indent
   npm --unsafe-perm prune 2>&1 | indent
